f36a08e45cf0d30d384d11984b3f5ae981ff4f82
JessBonanno <jessbonanno@gmail.com>
2021-04-14 15:17:02 -0400

    created reviewContainer folder

---
+++ client/src/components/reviewWidget/ReviewContainer.jsx
@@ -0,0 +1,11 @@
+import Reactfrom 'react';
+import styles from './reviewContainer.module.css';
+
+const ReviewContainer = () => {
+
+  return (
+    <div></div>
+  )
+}
+
+export default ReviewContainer;
