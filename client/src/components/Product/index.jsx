e17a35536193e63cce7284c3ba64e72cc19f00f3
Dmanthonyx <dmanthonycoding@gmail.com>
2021-04-15 16:09:41 -0700

    going to merge with updated remote master

---
+++ client/src/components/Product/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './product.module.css';
+
+const Product = () => {
+  return (
+    <div className='wrapper'>
+
+    </div>
+  )
+}
+
+export default Product;
