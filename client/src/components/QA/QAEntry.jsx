773b1912f210cb2aa2646d3017b7ae403cc5ed99
Daniel <d_suh95@yahoo.com>
2021-04-15 18:56:58 -0500

    Conditional rendering of answers functional

--- client/src/components/QA/QAEntry.jsx
+++ client/src/components/QA/QAEntry.jsx
@@ -1,24 +1,22 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect, useContext, useState} from 'react';
 import Answer from './QAEntryAnswers.jsx';
 import styles from './qa.module.css';
 
 var QA = (props) => {
+
+  const[clicked, setClicked] = useState(false);
+
   var answers = Object.entries(props.answers);
   var initialAnswers = answers.slice(0, 2);
-  var usedAnswers;
-  console.log(answers);
-  console.log(initialAnswers);
+
   var enoughAnswers = (answers.length > 2);
-  var clicked = false;
-  var clickAnswers = function() {
-    clicked = !clicked;
-    console.log(clicked);
-  }
+  var usedAnswers;
   if (clicked) {
     usedAnswers = answers;
   } else {
     usedAnswers = initialAnswers;
   }
+
   return (
     <div>
       <div>
@@ -29,8 +27,10 @@ var QA = (props) => {
           <Answer key={entry[1].id} answer={entry[1].body} date={entry[1].date} author={entry[1].answerer_name} helpfulness={entry[1].helpfulness} photos={entry[1].photos}/>
           )}
       </div>
-      {enoughAnswers
-        ? <button className={styles.feedbutton} onClick={() => {clickAnswers()}}>Load more answers</button>
+      {enoughAnswers ?
+        clicked
+          ? <button className={styles.feedbutton} onClick={() => setClicked(false)}>Collapse answers</button>
+          : <button className={styles.feedbutton} onClick={() => setClicked(true)}>Load more answers</button>
         : <div/>
       }
     </div>
