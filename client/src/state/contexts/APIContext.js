e3ee5cb7a250288a3df0da43a606c9d3190e72ae
JessBonanno <jessbonanno@gmail.com>
2021-04-15 19:53:22 -0400

    api context working with test endpoint

--- client/src/state/contexts/APIContext.js
+++ client/src/state/contexts/APIContext.js
@@ -1,16 +1,32 @@
 import React, {createContext, useContext, useState} from 'react';
+import axios from 'axios'
+import {REACT_APP_API_KEY} from '../../config/config.js';
 
 export const APIContext = createContext({});
 
 const APIProvider = ({children}) => {
 
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp'
+
+// sample request to get all products
+
+const getAllProducts = async () => {
+  try {
+    const products = await axios.get(`${baseURL}/products`, {
+      headers: {'Authorization': REACT_APP_API_KEY}
+    })
+    console.log(products)
+  }catch (err) {
+    console.log(err)
+  }
+}
 
 
 
   return (
     <APIContext.Provider
       value={{
-        
+        getAllProducts,
       }}
     >
     {children}
