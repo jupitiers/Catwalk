122e03d15b9aac5fabb58f261d0f66d46e24691f
jyxsul <jonn.sullins@gmail.com>
2021-04-16 18:21:17 -0500

    started your outfit basic styling

--- client/src/components/Related/YourOutfit/YourOutfitCarousel.jsx
+++ client/src/components/Related/YourOutfit/YourOutfitCarousel.jsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useContext} from 'react';
 import AddCard from './AddCard.jsx';
 import OutfitCard from './OutfitCard.jsx';
+import styles from './yourOutfitCarousel.module.css';
 
 const YourOutfitCarousel = () => {
   return (
-    <div>
-      <button>Left</button>
+    <div className={styles.carousel}>
+      <button className={styles.carouselButton}>Left</button>
       <AddCard />
       <OutfitCard />
       <OutfitCard />
       <OutfitCard />
-      <button>Right</button>
+      <button className={styles.carouselButton}>Right</button>
     </div>
   )
 }
