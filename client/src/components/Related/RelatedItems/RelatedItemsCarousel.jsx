aac2e80f9e2796b53c75ac8b9647b75b05c44a3f
jyxsul <jonn.sullins@gmail.com>
2021-04-19 17:25:17 -0500

    quick commit before switch

--- client/src/components/Related/RelatedItems/RelatedItemsCarousel.jsx
+++ client/src/components/Related/RelatedItems/RelatedItemsCarousel.jsx
@@ -5,6 +5,7 @@ import styles from './relatedItemsCarousel.module.css';
 const RelatedItemsCarousel = props => {
   return (
     <div className={styles.carousel}>
+
       <button className={styles.carouselButton}><i className="fas fa-angle-left"></i></button>
       {props.data.sampleRelatedId.map((id, index) => {
         return <RelatedItemCard key={index} relatedId={id} data={props.data}/>
