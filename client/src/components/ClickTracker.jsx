e5a4857bbcd50b25c04f2d74bbd7e2770c2a4bb6
JessBonanno <jessbonanno@gmail.com>
2021-04-23 17:03:21 -0400

    removed commment

--- client/src/components/ClickTracker.jsx
+++ client/src/components/ClickTracker.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { APIContext } from '../state/contexts/APIContext';
-// This component encapsulates the behavior we need...
+
 const ClickTracker = ({ render }) => {
   const { trackClick } = useContext(APIContext);
   return (
