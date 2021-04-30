3f873c35061235089de76957e80aaf861926ef58
JessBonanno <jessbonanno@gmail.com>
2021-04-30 13:59:21 -0400

    added cloudinary folder

--- client/src/components/QA/AnswerModal.jsx
+++ client/src/components/QA/AnswerModal.jsx
@@ -1,13 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import $ from 'jquery';
 import lightStyles from './qaLight.module.css';
 import darkStyles from './qaDark.module.css';
-import $ from 'jquery';
 
 import { APIContext } from '../../state/contexts/APIContext';
 import { ReviewContext } from '../../state/contexts/ReviewsContext';
 import { REACT_APP_CLOUDINARY_URL, REACT_APP_CLOUDINARY_PRESET } from '../../config/config';
-
-
 
 const AnswerModal = (props) => {
   const { getQuestionsByProductId, addAnswer, trackClick } = useContext(APIContext);
@@ -18,24 +16,26 @@ const AnswerModal = (props) => {
   const [submittable, setSubmittable] = useState(true);
   const [photos, setPhotos] = useState([]);
 
-  var answerSubmit = true;
-  var nicknameSubmit = true;
-  var emailSubmit = true;
+  let answerSubmit = true;
+  let nicknameSubmit = true;
+  let emailSubmit = true;
 
-  var addPhoto = function(imageUrl) {
-    var currentPhotos = photos.slice();
+  const addPhoto = function (imageUrl) {
+    const currentPhotos = photos.slice();
     if (currentPhotos.length < 5 && imageUrl) {
       currentPhotos.push(imageUrl);
       setPhotos(currentPhotos);
     } else if (currentPhotos.length === 5) {
       console.log('too many photos');
     }
-  }
+  };
 
-  var uploadPhoto = async (e) => {
+  const uploadPhoto = async (e) => {
     const formData = new FormData();
     formData.append('file', e.target.files[0]);
     formData.append('upload_preset', REACT_APP_CLOUDINARY_PRESET);
+    formData.append('folder', 'catwalk');
+
     try {
       const res = await fetch(REACT_APP_CLOUDINARY_URL, {
         method: 'POST',
@@ -53,7 +53,7 @@ const AnswerModal = (props) => {
     }
   };
 
-  var submit = function(questionId, data) {
+  const submit = function (questionId, data) {
     if (answerSubmit && nicknameSubmit && emailSubmit) {
       setSubmittable(true);
       addAnswer(questionId, data);
@@ -61,9 +61,9 @@ const AnswerModal = (props) => {
     } else {
       setSubmittable(false);
     }
-  }
+  };
 
-  var checkAuth = function(answer, nickname, email) {
+  const checkAuth = function (answer, nickname, email) {
     if (answer && answer.length > 0) {
       setAnswerAuth(true);
       answerSubmit = true;
@@ -88,95 +88,119 @@ const AnswerModal = (props) => {
       emailSubmit = false;
     }
 
-    var answerData = {
+    const answerData = {
       body: answer,
       name: nickname,
-      email: email,
-      photos: photos
-    }
+      email,
+      photos,
+    };
 
     submit(props.questionId, answerData);
-  }
+  };
 
-  return(
+  return (
     <div className={lightStyles.modalcontent}>
-      <h2 id='title'>Submit Your Answer</h2>
-      <h4 id='subtitle'>{props.productData.name}: {props.question}</h4>
+      <h2 id="title">Submit Your Answer</h2>
+      <h4 id="subtitle">
+        {props.productData.name}
+        :
+        {' '}
+        {props.question}
+      </h4>
       <div>
         <div>
           {submittable
             ? null
-            : <p className={lightStyles.submiterror}>You must enter the following: </p>
-          }
+            : <p className={lightStyles.submiterror}>You must enter the following: </p>}
         </div>
         <div className={lightStyles.modaldiv}>
           {answerAuth
-            ? <div>
-                <span>Your Answer: *</span><br/>
-                <textarea id='answer' className={lightStyles.modalquestion} maxLength='1000' placeholder='Write your answer here (1000 character max)'/>
+            ? (
+              <div>
+                <span>Your Answer: *</span>
+                <br />
+                <textarea id="answer" className={lightStyles.modalquestion} maxLength="1000" placeholder="Write your answer here (1000 character max)" />
               </div>
-            : <div>
-                <span className={lightStyles.modaltitlecheck}>Question: *</span><br/>
-                <textarea id='answerCheck' className={lightStyles.modalquestioncheck} maxLength='1000' placeholder='Write your answer here (1000 character max)'/>
+            )
+            : (
+              <div>
+                <span className={lightStyles.modaltitlecheck}>Question: *</span>
+                <br />
+                <textarea id="answerCheck" className={lightStyles.modalquestioncheck} maxLength="1000" placeholder="Write your answer here (1000 character max)" />
               </div>
-          }
+            )}
         </div>
         <div className={lightStyles.modaldiv}>
           {nicknameAuth
-            ? <div>
-                <span>What is your nickname: * </span><br/>
-                <input type='text' id='nickname' className={lightStyles.modaluser} maxLength='60' placeholder='Example: jack543!'/><br/>
+            ? (
+              <div>
+                <span>What is your nickname: * </span>
+                <br />
+                <input type="text" id="nickname" className={lightStyles.modaluser} maxLength="60" placeholder="Example: jack543!" />
+                <br />
                 <span>For privacy reasons, do not use your full name or email address</span>
               </div>
-            : <div>
-                <span className={lightStyles.modaltitlecheck}>Nickname: * </span><br/>
-                <input type='text' id='nicknameCheck' className={lightStyles.modalusercheck} maxLength='60' placeholder='Example: jack543!'/><br/>
+            )
+            : (
+              <div>
+                <span className={lightStyles.modaltitlecheck}>Nickname: * </span>
+                <br />
+                <input type="text" id="nicknameCheck" className={lightStyles.modalusercheck} maxLength="60" placeholder="Example: jack543!" />
+                <br />
                 <span>For privacy reasons, do not use your full name or email address</span>
               </div>
-          }
+            )}
         </div>
         <div className={lightStyles.modaldiv}>
           {emailAuth
-            ? <div>
-                <span>Your email: * </span><br/>
-                <input type='text' id='email' className={lightStyles.modaluser} maxLength='60' placeholder='Example: jack@email.com'/><br/>
+            ? (
+              <div>
+                <span>Your email: * </span>
+                <br />
+                <input type="text" id="email" className={lightStyles.modaluser} maxLength="60" placeholder="Example: jack@email.com" />
+                <br />
                 <span>For authentication purposes, you will not be emailed</span>
               </div>
-            : <div>
-                <span className={lightStyles.modaltitlecheck}>Email: * </span><br/>
-                <input type='text' id='emailCheck' className={lightStyles.modalusercheck} maxLength='60' placeholder='Example: jack@email.com'/><br/>
+            )
+            : (
+              <div>
+                <span className={lightStyles.modaltitlecheck}>Email: * </span>
+                <br />
+                <input type="text" id="emailCheck" className={lightStyles.modalusercheck} maxLength="60" placeholder="Example: jack@email.com" />
+                <br />
                 <span>For authentication purposes, you will not be emailed</span>
               </div>
-          }
+            )}
         </div>
         <div className={lightStyles.modaldiv}>
           <div>
-            <span>Upload your photos: </span><br/>
-            <input type='text' id='photoUrl' className={lightStyles.modalphotos} placeholder='Place your photo URL here'/>
+            <span>Upload your photos: </span>
+            <br />
+            <input type="text" id="photoUrl" className={lightStyles.modalphotos} placeholder="Place your photo URL here" />
             {photos.length < 5
-              ? <button className={lightStyles.addphoto} onClick={() => {addPhoto($('#photoUrl').val())}}>Add photo</button>
-              : <span>Max photos added</span>
-            } <br/>
+              ? <button className={lightStyles.addphoto} onClick={() => { addPhoto($('#photoUrl').val()); }}>Add photo</button>
+              : <span>Max photos added</span>}
+            {' '}
+            <br />
             {photos.length < 5
-              ? <label className={lightStyles.uploadphoto}>
+              ? (
+                <label className={lightStyles.uploadphoto}>
                   Upload a photo
-                  <input type="file" id='photoUpload' placeholder='Upload a photo' onChange={uploadPhoto}/>
+                  <input type="file" id="photoUpload" placeholder="Upload a photo" onChange={uploadPhoto} />
                 </label>
-              : <span>Max photos added</span>
-            }
+              )
+              : <span>Max photos added</span>}
             <div>
-              {photos.map(url =>
-                <img className={lightStyles.answerimage} key={url} src={url}/>
-              )}
+              {photos.map((url) => <img className={lightStyles.answerimage} key={url} src={url} />)}
             </div>
           </div>
         </div>
         <div>
-          <button id='answersubmit' className={lightStyles.questionsubmit} onClick={() => {checkAuth($('#answer').val(), $('#nickname').val(), $('#email').val())}}>Submit answer</button>
+          <button id="answersubmit" className={lightStyles.questionsubmit} onClick={() => { checkAuth($('#answer').val(), $('#nickname').val(), $('#email').val()); }}>Submit answer</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AnswerModal;
