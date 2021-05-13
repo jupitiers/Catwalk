ff06b69853cc1823f69bedef16ff0819d446ee7a
Marco Canchola <marcocanchola2020@gmail.com>
2021-05-12 21:26:59 -0700

    Updated database client conn

--- server/reviews/database/index.js
+++ server/reviews/database/index.js
@@ -1,10 +1,10 @@
 const cassandra = require('cassandra-driver');
-// const host = process.env.CASSANDRA_HOST;
+const host = process.env.CASSANDRA_HOST;
 const username = process.env.CASSANDRA_USERNAME;
 const password = process.env.CASSANDRA_PASSWORD;
 
 const client = new cassandra.Client({
-  contactPoints: ['40.87.123.102'],
+  contactPoints: [host],
   localDataCenter: 'datacenter1',
   credentials: { username: username, password: password }
 });
