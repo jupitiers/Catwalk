f9ca156387289c207e8295b0cdc84c69b5555b8d
Marco Canchola <marcocanchola2020@gmail.com>
2021-05-12 19:53:26 -0700

    Updated variables

--- server/reviews/database/index.js
+++ server/reviews/database/index.js
@@ -1,10 +1,10 @@
 const cassandra = require('cassandra-driver');
-const host = process.env.CASSANDRA_HOST;
+// const host = process.env.CASSANDRA_HOST;
 const username = process.env.CASSANDRA_USERNAME;
 const password = process.env.CASSANDRA_PASSWORD;
 
 const client = new cassandra.Client({
-  contactPoints: [host],
+  contactPoints: ['40.87.123.102'],
   localDataCenter: 'datacenter1',
   credentials: { username: username, password: password }
 });
