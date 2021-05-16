4b384380bbbaec23871086b6132143ca1ba9fce5
Marco Canchola <marcocanchola2020@gmail.com>
2021-05-16 16:47:26 -0700

    Implemented caching to api

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(__dirname + '/loader'));
 app.use('/reviews', reviewsRouter);
 
 
--- server/reviews/routes/reviews.route.js
+++ server/reviews/routes/reviews.route.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const cassandraClient = require('../database/index.js');
+const cassandraDriver = require('cassandra-driver');
+const executeConcurrent = cassandraDriver.concurrent.executeConcurrent;
+const Cache = require('mem-cache');
+const cache = new Cache();
 
 // Get reviews based off of product id
 router.get('/', async (req, res) => {
   try {
+    // check to see if the cache is there
     const { product_id } = req.query;
-    const page = req.query.page || 1;
-    const count = req.query.count || 5 ;
-    const getReviewsByProductIdQuery = 'select * from reviews.reviews where product_id = ?';
-    const reviews = await cassandraClient.execute(getReviewsByProductIdQuery, [product_id], { prepare: true });
-    // filter our reported
-    const filteredReviews = reviews.rows.filter(review => review.reported !== true);
-    const results = {};
-    results.product;
-    results.page = page;4
-    results.count = count;
-    results.results = filteredReviews;
-    res.status(200).json({ ...results });
-    // create a readable stream for reviews
+    const cachedProduct = cache.get(product_id);
+    if (cachedProduct) {
+      res.status(200).json({...cachedProduct});
+    } else {
+      const page = req.query.page || 1;
+      const count = req.query.count || 5 ;
+      const getReviewsByProductIdQuery = 'select * from reviews.reviews where product_id = ?';
+      const reviews = await cassandraClient.execute(getReviewsByProductIdQuery, [product_id], { prepare: true });
+      // filter our reported
+      const filteredReviews = reviews.rows.filter(review => review.reported !== true);
+      const results = {};
+      results.product;
+      results.page = page;
+      results.count = count;
+      results.results = filteredReviews;
+      // set product to cache
+      cache.set(product_id, results);
+      res.status(200).json({ ...results });
+    }
   } catch (err) {
     console.log(err)
     res.status(500).json({ success: false, message: 'Failed getting reviews...' });
@@ -29,80 +40,97 @@ router.get('/', async (req, res) => {
 router.get('/meta/', async (req, res) => {
   const { product_id } = req.query;
   try {
-    const characteristicObj = { product_id: product_id };
-    // Calculate all of the ratings
-    const getRatings = await cassandraClient.execute('select rating from reviews.reviews where product_id = ?', [product_id], { prepare: true });
-    const ratings = { '1': 0, '2': 0, '3': 0, '4': 0, '5': 0 };
-    // Iterate thru all the ratings and increment
-    for (let obj of getRatings.rows) {
-      if (ratings.hasOwnProperty(obj.rating)) {
-        ratings[obj.rating] += 1;
+    // check to see if the meta-data for product is cached
+    const cachedProduct = cache.get(product_id+'meta');
+    if (cachedProduct) {
+      res.status(200).json({...cachedProduct});
+    } else {
+      const characteristicObj = { product_id: product_id };
+      // Create concurrency level for getting ratings, recommends, and characteristics
+      // based on product id
+      const getRelevantMetaData = [
+        { query: 'select rating from reviews.reviews where product_id = ?',
+          params: [ product_id ] },
+        { query: 'select recommend from reviews.reviews where product_id = ?',
+          params: [ product_id ] },
+      ];
+      // create concurrent
+      const concurrentData = await executeConcurrent(cassandraClient, getRelevantMetaData, { collectResults: true });
+      const [ ratingsMeta, recommendMeta, characteristicsMeta ] = concurrentData.resultItems;
+
+      // Calculate all of the ratings
+      // const getRatings = await cassandraClient.execute('select rating from reviews.reviews where product_id = ?', [product_id], { prepare: true });
+      const ratings = { '1': 0, '2': 0, '3': 0, '4': 0, '5': 0 };
+      // Iterate thru all the ratings and increment
+      for (let obj of ratingsMeta.rows) {
+        if (ratings.hasOwnProperty(obj.rating)) {
+          ratings[obj.rating] += 1;
+        }
       }
-    }
-    characteristicObj['ratings'] = ratings;
+      characteristicObj['ratings'] = ratings;
 
-    // Calculate all of the recommended from reviews
-    const getRecommends = await cassandraClient.execute('select recommend from reviews.reviews where product_id = ?', [product_id], { prepare: true });
-    const recommend = { 'true': 0, 'false': 0 };
-    // Iterate thru all the ratings and increment
-    for (let obj of getRecommends.rows) {
-      if (recommend.hasOwnProperty(obj.recommend)) {
-        recommend[obj.recommend] += 1;
+      // Calculate all of the recommended from reviews
+      // const getRecommends = await cassandraClient.execute('select recommend from reviews.reviews where product_id = ?', [product_id], { prepare: true });
+      const recommend = { 'true': 0, 'false': 0 };
+      // Iterate thru all the ratings and increment
+      for (let obj of recommendMeta.rows) {
+        if (recommend.hasOwnProperty(obj.recommend)) {
+          recommend[obj.recommend] += 1;
+        }
       }
-    }
-    characteristicObj['recommend'] = recommend;
+      characteristicObj['recommend'] = recommend;
 
-    const characteristcQuery = 'select * from reviews.characteristics where product_id = ?';
-    let characteristicArr = [];
-    // Create a read strean for Cassandra's queries
-    await cassandraClient.stream(characteristcQuery, [product_id], { prepare: true })
-      .on('readable', function () {
-        // 'readable' is emitted as soon a row is received and parsed
-        var row;
-        while (row = this.read()) {
-          // push each characteristic to row
-          characteristicArr.push(row);
-        }
-      })
-      .on('end', async function () {
-        // Stream ended, there aren't any more rows
-        const characteristicMeta = [];
-        for (let characteristic of characteristicArr) {
-          // intialize the characteristic object by each type
-          const obj = {
-            [characteristic.name]: true
-          };
-          const value = await cassandraClient.execute('select * from reviews.characteristics_reviews where characteristic_id = ?', [characteristic.id], { prepare: true });
-          let sum = 0;
-          let lenOfChar = value.rows.length;
-          for (let row of value.rows) {
-            // Find the average for characteristic
-            sum += row.value / lenOfChar;
+      const characteristcQuery = 'select * from reviews.characteristics where product_id = ?';
+      let characteristicArr = [];
+      // Create a read strean for Cassandra's queries
+      await cassandraClient.stream(characteristcQuery, [product_id], { prepare: true })
+        .on('readable', function () {
+          // 'readable' is emitted as soon a row is received and parsed
+          var row;
+          while (row = this.read()) {
+            // push each characteristic to row
+            characteristicArr.push(row);
           }
-          // check to see if the characteristic exists
-          if (obj.hasOwnProperty(characteristic.name)) {
-            obj['value'] = sum;
-            obj['name'] = characteristic.name;
-            obj['id'] = characteristic.id;
+        })
+        .on('end', async function () {
+          // Stream ended, there aren't any more rows
+          const characteristicMeta = [];
+          for (let characteristic of characteristicArr) {
+            // intialize the characteristic object by each type
+            const obj = {
+              [characteristic.name]: true
+            };
+            const value = await cassandraClient.execute('select * from reviews.characteristics_reviews where characteristic_id = ?', [characteristic.id], { prepare: true });
+            let sum = 0;
+            let lenOfChar = value.rows.length;
+            for (let row of value.rows) {
+              // Find the average for characteristic
+              sum += row.value / lenOfChar;
+            }
+            // check to see if the characteristic exists
+            if (obj.hasOwnProperty(characteristic.name)) {
+              obj['value'] = sum;
+              obj['name'] = characteristic.name;
+              obj['id'] = characteristic.id;
+            }
+            characteristicMeta.push(obj);
           }
-          characteristicMeta.push(obj);
-        }
 
-        characteristicObj['characteristics'] = {};
-        for (let characteristic of characteristicMeta) {
-          characteristicObj['characteristics'][characteristic.name] = {
-            id: characteristic.id,
-            value: characteristic.value
+          characteristicObj['characteristics'] = {};
+          for (let characteristic of characteristicMeta) {
+            characteristicObj['characteristics'][characteristic.name] = {
+              id: characteristic.id,
+              value: characteristic.value
+            }
           }
-        }
-
-        res.status(200).json({ ...characteristicObj });
-      })
-      .on('error', function (err) {
-        // Something went wrong: err is a response error from Cassandra
-        console.log('Streaming failed with Cassandra', err);
-      });
-
+          cache.set(product_id+'meta', characteristicObj);
+          res.status(200).json({ ...characteristicObj });
+        })
+        .on('error', function (err) {
+          // Something went wrong: err is a response error from Cassandra
+          console.log('Streaming failed with Cassandra', err);
+        });
+      }
   } catch (err) {
     console.log(err)
     res.status(500).json({ success: false, message: `Failed getting meta-data for product id ${product_id}` });
@@ -170,6 +198,7 @@ router.put('/:review_id/helpful', async (req, res) => {
     // increment review's helpfulness
     const newReviewHelpfulness = review.rows[0].helpfulness + 1;
     // update review's helpfulness
+
     await cassandraClient.execute('update reviews.reviews set helpfulness = ? where product_id = ? and id = ?', [newReviewHelpfulness, reviewProductsRow.rows[0].product_id, review.rows[0].id], { prepare: true });
     res.status(200).json({ success: true, message: 'Successfully marked review helpful.' });
   } catch (err) {
